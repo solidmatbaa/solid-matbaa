4c0362d29e3eeb9f2ff0aa78e3385ade460cb462
mo3tasm ghazal <mo3tasm.ghazalgmail.com@192.168.1.102>
2026-07-01 04:42:52 +0300

    fix env vars

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
