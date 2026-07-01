fbe74d60c2762a24a224ddffd78653c655ab1bab
mo3tasm ghazal <mo3tasm.ghazalgmail.com@192.168.1.102>
2026-07-01 05:21:24 +0300

    Clean up config file

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
+import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+
 const nextConfig: NextConfig = {
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: "https://gncohtnoqlvdfuzqyyk.supabase.co",
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: "هنا_ضع_مفتاح_الـ_anon_الذي_نسخته",
-    SUPABASE_SERVICE_ROLE_KEY: "هنا_ضع_مفتاح_الـ_service_role_الذي_يبدأ_بـ_ey",
-  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
@@ -11,3 +11,5 @@ const nextConfig: NextConfig = {
     ],
   },
 };
+
+export default withNextIntl(nextConfig);
