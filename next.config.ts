8bad358764b85e8c528987b3dc5ba8d05f81b0ad
mo3tasm ghazal <mo3tasm.ghazalgmail.com@192.168.1.102>
2026-07-01 04:52:42 +0300

    update env settings

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
-import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
-
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    NEXT_PUBLIC_SUPABASE_URL: "https://gncohtnoqlvdfuzqyyk.supabase.co",
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: "هنا_ضع_مفتاح_الـ_anon_الذي_نسخته",
+    SUPABASE_SERVICE_ROLE_KEY: "هنا_ضع_مفتاح_الـ_service_role_الذي_يبدأ_بـ_ey",
   },
   images: {
     remotePatterns: [
@@ -15,5 +11,3 @@ const nextConfig: NextConfig = {
     ],
   },
 };
-
-export default withNextIntl(nextConfig);
